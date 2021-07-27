0a7aabe04885f5af33f04d3b8df042e85ce2eb91
hamad063 <haamad063@gmail.com>
2021-07-27 18:11:29 +0300

    create new django project , add posts app

--- post/views.py
+++ post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from . models import Post
+
+
+def post_list(request):
+    all_posts = Post.objects.all()
+    return render(request,'post/post_list.html',{'posts':all_posts})
--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from post.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post/' , post_list)
 ]
